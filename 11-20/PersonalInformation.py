043451c08ee8ce60216da1e4ea234365fd95bd17
Abraham <abraham@19cah.com>
2018-02-10 01:34:18 -0500

    add no. 12

---
+++ 11-20/PersonalInformation.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Write a program that displays the
+# following information, each on a separate line:
+#
+#   Your name
+#   Your address, with city, state, and zip code
+#   Your telephone number
+#   Your college major
+#
+# Author: Carlos Abraham, @19cah
+# www.19cah.com
+
+name = 'Abraham'
+direction = '123 N Test Street, Miami FL, 12345'
+telephoneNumber = 3051234567
+major = 'Computer Science'
+
+print "Name: ", name
+print "Address: ", direction
+print "Phone: ", telephoneNumber
+print "Major: ", major
